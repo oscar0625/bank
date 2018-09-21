7cedabe64aa5c886f99757fcc8cf627cd9cbf714
oscar0625 <749301111@qq.com>
2018-09-21 16:36:06 +0800

    180921_1.1.2

---
+++ work/web/script/websiteStatistics.js
@@ -0,0 +1,9 @@
+define(function () {
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?bcea94fd92614066059025c5afcc9eca";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+})
