08509067dfd657253929d293125aa27eb2a6efe4
oscar0625 <749301111@qq.com>
2018-12-07 17:39:53 +0800

    181207_1.3.5

--- bank/polyfill.js
+++ bank/polyfill.js
@@ -186,4 +186,10 @@
         };
     }
 
+    //String.trim()
+    if (!String.prototype.trim) {
+        String.prototype.trim = function () {
+            return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+        };
+    }
 })()
