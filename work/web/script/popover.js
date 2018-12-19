426e79a20c0d2877227f9c68dda52ef91dc306be
oscar0625 <749301111@qq.com>
2018-12-19 14:22:31 +0800

    181219_1.3.9

--- work/web/script/popover.js
+++
@@ -1,8 +0,0 @@
-define(['dialog'],function (dialog) {
-    return {
-        //弹出框
-        message:function (content) {
-            dialog({title: '提示信息', content:content, width:300, height:50,skin:'artdialogCenter'}).showModal()
-        }
-    }
-});
--- work/web/script/websiteStatistics.js
+++
@@ -1,9 +0,0 @@
-define(function () {
-    var _hmt = _hmt || [];
-    (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?bcea94fd92614066059025c5afcc9eca";
-        var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(hm, s);
-    })();
-})
