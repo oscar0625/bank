c6e071d977ba0c839dfe9eade0d07b66d517a7ee
oscar0625 <749301111@qq.com>
2020-02-27 17:33:30 +0800

    200227_2.0.8

--- 插件/tinymce/editor.vue
+++ 插件/tinymce/editor.vue
@@ -15,6 +15,7 @@ import "tinymce/themes/silver/theme";
 import Editor from "@tinymce/tinymce-vue";
 import "tinymce/plugins/code";
 import "tinymce/plugins/link";
+import "tinymce/plugins/advlist";
 import "tinymce/plugins/lists";
 import "tinymce/plugins/image";
 import "tinymce/plugins/table";
@@ -22,6 +23,7 @@ import "tinymce/plugins/paste";
 import "tinymce/plugins/preview";
 import "tinymce/plugins/fullscreen";
 import "tinymce/plugins/wordcount";
+import "tinymce/plugins/lineheight";
 
 export default {
   name: "tinymceEditor",
@@ -65,9 +67,14 @@ export default {
 
         menubar: true, // 隐藏最上方menu
         plugins:
-          "code link lists image table paste preview fullscreen wordcount",
-        toolbar:
-          "code | fontsizeselect bold italic underline strikethrough forecolor backcolor | alignleft aligncenter alignright alignjustify | image table link unlink  | h1 h2 h3 | outdent indent bullist numlist | preview fullscreen wordcount |removeformat",
+          "code link advlist lists image table paste preview fullscreen wordcount lineheight",
+        toolbar: [
+          "code undo redo restoredraft | bold italic underline strikethrough forecolor backcolor | lineheight alignleft aligncenter alignright alignjustify | bullist numlist blockquote subscript superscript removeformat",
+          " formatselect fontselect fontsizeselect | image table link unlink | outdent indent| preview fullscreen wordcount "
+        ],
+        fontsize_formats: "12px 14px 16px 18px 24px 36px 48px 56px 72px",
+        font_formats:
+          "微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC,sans-serif;苹果苹方=PingFang SC,Microsoft YaHei,sans-serif;宋体=simsun,serif;仿宋体=FangSong,serif;黑体=SimHei,sans-serif;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats;知乎配置=BlinkMacSystemFont, Helvetica Neue, PingFang SC, Microsoft YaHei, Source Han Sans SC, Noto Sans CJK SC, WenQuanYi Micro Hei, sans-serif;小米配置=Helvetica Neue,Helvetica,Arial,Microsoft Yahei,Hiragino Sans GB,Heiti SC,WenQuanYi Micro Hei,sans-serif",
         //上传图片 有2种方式
         images_upload_handler: function(blobInfo, success, failure) {
           //1.接上传base64
